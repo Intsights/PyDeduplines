c5e3c3b2861dc9e0492e1d038cd72688cadf64cc
Gal Ben David <gal@intsights.com>
2020-08-24 13:06:57 +0300

    wip

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import glob
 
 setuptools.setup(
     name='PyDeduplines',
-    version='0.1.5',
+    version='0.2.0',
     author='Gal Ben David',
     author_email='gal@intsights.com',
     url='https://github.com/intsights/PyDeduplines',
